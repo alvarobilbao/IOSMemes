5c66efd71731f4cf6b86af49c3bc278f4e2eb400
Alvaro Bilbao La Vieja <alvaro@alvaros-imac.local>
2018-05-16 15:36:51 -0700

    creating necessary files

---
+++ Movies/MovieDescriptionController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDescriptionController.swift
+//  Movies
+//
+//  Created by Alvaro Bilbao La Vieja on 16/05/2018.
+//  Copyright © 2018 Alvaro Bilbao La Vieja. All rights reserved.
+//
+
+import UIKit
+
+class MovieDescriptionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

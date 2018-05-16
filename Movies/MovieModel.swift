731121c93656cac7bebfc63d57de378f1d7e5f61
Alvaro Bilbao La Vieja <alvaro@alvaros-imac.local>
2018-05-16 15:39:50 -0700

    movie model

---
+++ Movies/MovieModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieModel.swift
+//  Movies
+//
+//  Created by Alvaro Bilbao La Vieja on 16/05/2018.
+//  Copyright © 2018 Alvaro Bilbao La Vieja. All rights reserved.
+//
+
+import Foundation

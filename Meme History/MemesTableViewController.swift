13c15f04f66041f8f37264143a967d564bf2f9a0
Alvaro Bilbao La Vieja <alvaro@alvaros-imac.local>
2018-05-31 04:13:24 -0700

    images, descriptions and launch screen

--- Meme History/MemesTableViewController.swift
+++ Meme History/MemesTableViewController.swift
@@ -13,13 +13,19 @@ class MemesTableViewController: UITableViewController {
     var memesArr = [MemeModel]()
     var selectedMeme : MemeModel?
     
-    let meme1 = MemeModel(name:"why not?", year:"2012", description:"desc", img: "whyNot")
-    let meme2 = MemeModel(name:"Im watchin you", year:"2012", description:"desc", img: "imwatchingyou")
+    let meme1 = MemeModel(name:"Expand your brain", year:"2017", description:"Expanding Brain, also known as Galaxy Brain, is a multi-panel exploitable image series comparing the brain size of a person relative to other variables. Though the expanding brain is usually implied to showcase intellectual superiority over various objects, it is more often used in an ironic sense to imply the opposite, where objects of derision are implied to be of higher standard than objects that are usually highly regarded.", img: "brain")
+    
+    let meme2 = MemeModel(name:"Distracted Boyfriend", year:"2017", description:"Distracted Boyfriend, also known as Man Looking at Other Woman, is an object labeling stock photo series in which a man looks at the backside of a woman walking by while another woman, presumably his romantic partner, looks on disapprovingly.", img: "cheatMeme")
+    
     let meme3 = MemeModel(name:"Pedobear", year:"2003", description:"Pedobear is a cartoon mascot that became a well-known icon through its usage on 4chan to signal moderators and other users that illegal pornographic content had been posted. Due to the widespread nature of its application, Pedobear has been often misinterpreted as a symbol of pedophilia and lolita complex, especially in the news media and law enforcement agencies. \n\n The character known today as Pedobear used to be simply known as Kumar or Kuma (bear) on the Japanese textboard 2channel.", img: "pedobear")
+    
+    let meme4 = MemeModel(name: "Roll Safe", year: "2016", description: "Roll Safe is an image macro serious featuring a screenshot of actor Kayode Ewumi grinning and pointing to his temple while portraying the character Reece Simpson (a.k.a. Roll Safe) in the web series Hood Documentary. The image is often captioned with various jokes mocking poor decision making and failures in critical thinking.", img: "goodIdea")
+    
+    let meme5 = MemeModel(name: "It's A Trap!", year: "2000", description: "It's A Trap! is a catchphrase that is often used as a reaction image to photos of transsexuals and crossdressers (often referred to as traps), or people who appear sexually ambiguous. It usually means that the person in question has male reproductive organs, regardless of their appearance. Though less frequently, the phrase can be also used as a warning of any potential danger.", img: "itsATrap")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        memesArr = [meme1, meme2, meme3]
+        memesArr = [meme1, meme2, meme3, meme4, meme5]
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
